e8bff4bb475dd73e58714bd8fe24be1bcb09fefe
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-05 12:19:53 -0400

    .each loop with {} sintax

--- prime.rb
+++ prime.rb
@@ -3,8 +3,6 @@ def prime?(integer)
   return false if integer <= 1
 
   range = (2..Math.sqrt(integer).floor).to_a
-  range.each do |enumerator|
-      return false if integer % enumerator == 0
-  end
+  range.each {|enumerator|return false if integer % enumerator == 0}
   true
 end
